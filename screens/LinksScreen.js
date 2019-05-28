c14aee401b8d59f5719072dbc846ce0bccac1798
Ryan Doner <ryan.doner@techtonic.com>
2019-05-28 16:49:48 -0600

    add react native elements. Display match cards on linksScreen. Show flatlist of imgs on settings screen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,18 +1,62 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React from "react";
+import { ScrollView, StyleSheet } from "react-native";
+import { Card, ListItem, Avatar } from "react-native-elements";
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    header: null
   };
 
   render() {
+    const users = [
+      {
+        name: "Carl",
+        avatar: "https://theblogblogdotblog.files.wordpress.com/2018/04/1500days.jpeg"
+      },
+      {
+        name: "Brynn",
+        avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg"
+      },
+      {
+        name: "Paula",
+        avatar:
+          "https://2gz00p44tlr52qejv22mr7p2-wpengine.netdna-ssl.com/wp-content/uploads/2017/01/Paula-Pant-Afford-Anything-and-Will-Sisk-and-PT.jpg"
+      },
+      {
+        name: "Carl",
+        avatar: "https://theblogblogdotblog.files.wordpress.com/2018/04/1500days.jpeg"
+      },
+      {
+        name: "Brynn",
+        avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg"
+      },
+      {
+        name: "Paula",
+        avatar:
+          "https://2gz00p44tlr52qejv22mr7p2-wpengine.netdna-ssl.com/wp-content/uploads/2017/01/Paula-Pant-Afford-Anything-and-Will-Sisk-and-PT.jpg"
+      }
+    ];
+
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        {users.map((u, i) => {
+          return (
+            <Card key={i} containerStyle={{ padding: 0 }}>
+              <ListItem
+                // key={i}
+                title={u.name}
+                leftAvatar={
+                  <Avatar
+                    rounded
+                    source={{
+                      uri: u.avatar
+                    }}
+                  />
+                }
+              />
+            </Card>
+          );
+        })}
       </ScrollView>
     );
   }
@@ -21,7 +65,7 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    // paddingTop: 15,
+    backgroundColor: "#fff"
+  }
 });
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,97 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React from "react";
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  FlatList
+} from "react-native";
+import { WebBrowser } from "expo";
+import TabBarIcon from "../components/TabBarIcon";
+import Colors from "../constants/Colors";
+import { Input } from "react-native-elements";
 
+const photosToDisplay = [
+  {
+    img: 1,
+    src: ""
+  }
+];
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    header: null
+  };
+
+  renderItem = item => {
+    <Image
+      style={styles.gridImg}
+      source={require("../assets/images/mrMoneyMustache.jpg")}
+      resizeMode="contain"
+    />;
   };
 
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          {/* <FlatList
+            data={[1, 2]}
+            style={styles.container}
+            renderItem={({item}) => {
+              <Image
+                style={styles.gridImg}
+                source={require("../assets/images/mrMoneyMustache.jpg")}
+                resizeMode="contain"
+              />
+            }}
+            numColumns={3}
+          /> */}
+          <FlatList
+            data={[{ key: "a" }, { key: "b" }]}
+            numColumns={3}
+            renderItem={({ item }) => (
+              <Image
+                style={styles.gridImg}
+                source={require("../assets/images/mrMoneyMustache.jpg")}
+                resizeMode="contain"
+              />
+            )}
+          />
+          <View style={styles.userInfoContainer}>
+            <Input style={styles.userInfoText} label="Name" placeholder="Ryan" />
+            <Input style={styles.userInfoText} label="Age" placeholder="26" />
+          </View>
+        </ScrollView>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  contentContainer: {
+    // paddingTop: 30
+  },
+  imgContainer: {
+    flex: 1
+  },
+  gridImg: {
+    height: 200,
+    width: 200,
+    padding: 2
+  },
+  userInfoContainer: {
+    fontSize: 60
+  },
+  userInfoText: {
+    fontSize: 60
+  }
+});
